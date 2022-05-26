0fa2a5458b8be4479c66acb3f81f3ffe4330346c
Alexsander <alexsandergutierreez@gmail.com>
2022-05-25 21:28:36 -0300

    Aula

--- API_BANCO/Conta.cs
+++ API_BANCO/Conta.cs
@@ -11,5 +11,19 @@ namespace API_BANCO
         public long Numero { get; set; }
         public string Correntista { get; set; }
         public double Saldo { get; set; }
+
+        public Conta(long Numero, string Correntista, double Saldo)
+        {
+            this.Numero = Numero;
+            this.Correntista = Correntista;
+            this.Saldo = Saldo;
+        }
+        public void Creditar(double valor)
+        {
+            this.Saldo += valor;
+            Console.WriteLine("CREDITO: {0:C2}", valor);
+            Console.WriteLine("SALDO: {0:C2}", this.Saldo);
+        }
+        public abstract void Debitar(string valor);
     }
 }
